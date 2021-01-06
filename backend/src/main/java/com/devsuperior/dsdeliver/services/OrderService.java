08f04d8277a7c4845ee1ef9c1a1f348ecdb635c6
leandrobrito <leandrob549@hotmail.com>
2021-01-05 22:14:07 -0300

    Put /Orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,4 +41,14 @@ public class OrderService {
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.delivered);
+		order = repository.save(order);
+		return new OrderDTO(order);
+		
+	}
+
 }

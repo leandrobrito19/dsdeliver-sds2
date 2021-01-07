c12503839130e78125fa4e58bf52ad9299d1345d
leandrobrito <leandrob549@hotmail.com>
2021-01-07 17:39:46 -0300

    Add Routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,24 +1,24 @@
 import './styles.css';
 import {ReactComponent as YoutubeIcon} from './youtube.svg';
 import {ReactComponent as LinkedinIcon} from './linkedin.svg';
-import {ReactComponent as InstragramIcon} from './instagram.svg';
+import {ReactComponent as InstagrmIcon} from './youtube.svg';
 function Footer(){
     return (
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.youtube.com/devsuperior" target="_new">
-                     <YoutubeIcon />
-                </a>
-                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
-                    <LinkedinIcon />
-                </a>
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
-                    <InstragramIcon />
-                </a>
+            <a href="https://www.youtube.com/devsuperior" target="_new">
+            <YoutubeIcon />
+            </a>
+            <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+            <LinkedinIcon />
+            </a>
+            <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+            <InstagrmIcon />
+            </a>
             </div>
         </footer>
-    );
+    )
 }
 
 export default Footer;
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+
+import StepsHeader from './StepHeads';
+import './Styles.css';
+
+function Orders(){
+    return (
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders;
